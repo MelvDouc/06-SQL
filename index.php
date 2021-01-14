526e35b728a6a1d964819d7bca9e84553886488f
MelvDouc <melv.douc@gmail.com>
2021-01-14 14:15:06 +0100

    Bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<h1>Test</h1>
+<?php
+    require_once('./includes/bdd.inc.php');
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+    
+    <head>
+        <?php
+            // <head>
+            require('./templates/head.inc.html');
+        ?>
+    </head>
+
+    <body>
+
+        <header>
+            <?php
+                require('./templates/header.html');
+            ?>
+        </header>
+        
+        <main>
+            <?php
+
+            ?>
+        </main>
+
+        <footer class="position-absolute bottom-0 start-50 translate-middle-x">
+            <?php
+                require('./templates/footer.html');
+            ?>
+        </footer>
+    </body>
+
+</html>
